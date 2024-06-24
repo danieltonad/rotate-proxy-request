7bf93cb552d5b01384f2a3fa871b38f21fe5db80
danieltonad <soladan2000@gmail.com>
2024-06-24 11:41:59 -0900

    done playing

--- thread_play.py
+++ thread_play.py
@@ -1,9 +1,9 @@
 from time import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-start = time()
-milli = [i for i in range(1_000_000_000)]
-sec =  time() - start
+# start = time()
+# milli = [i for i in range(1_000_000_000)]
+# sec =  time() - start
 
 count = 0
 
@@ -17,6 +17,11 @@ def count_billion():
 #     print(f"{count:,}", end='\r')
 # sec =  time() - start
 
-
-
+start = time()
+with ThreadPoolExecutor(max_workers=500) as executor:
+    # futures = [executor.submit(count_billion) for i in range(1_000_000)]
+    for i in  range(1_000_000):
+        executor.submit(count_billion)
+sec =  time() - start
+print(count)
 print(f"{sec:.2f} sec")
